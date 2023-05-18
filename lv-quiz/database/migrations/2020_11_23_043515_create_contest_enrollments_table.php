8156d87818408d506af6df1e6254cfa9d9e60c55
Oishy Barua <68017863+oishycmt9@users.noreply.github.com>
2023-05-18 11:57:25 +0600

    Update 2020_11_23_043515_create_contest_enrollments_table.php

--- lv-quiz/database/migrations/2020_11_23_043515_create_contest_enrollments_table.php
+++ lv-quiz/database/migrations/2020_11_23_043515_create_contest_enrollments_table.php
@@ -4,30 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-CreateContestEnrollmentsTable extends Migration
+class CreateContestEnrollmentsTable extends Migration
 {
     /**
      * Run the migrations.
